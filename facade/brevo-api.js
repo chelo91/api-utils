7211cb9b0e8d1099caee022c9eb57f35447adbcb
Chelo <me@chelo.xyz>
2024-04-27 13:00:07 -0500

    changes in sender

--- facade/brevo-api.js
+++ facade/brevo-api.js
@@ -1,21 +1,20 @@
 import SibApiV3Sdk from '@getbrevo/brevo';
-import { brevoKey } from '../config/const.config.js';
-import { resOk, resError } from '../helper/response.js';
+import { brevoKey, mailSender } from '../config/const.config.js';
 
 const sendEmail = async (subject, body, to) => {
+    if (!subject || !body || !to) {
+        throw new Error('"Subject", "body" and "to" are required');
+    }
+
     const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
     const apiKey = apiInstance.authentications['apiKey'];
     apiKey.apiKey = brevoKey;
     const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
-    if (!subject || !body || !to) {
-        throw new Error('Subject, body and to are required');
-    }
-
     sendSmtpEmail.subject = `${subject}`;
     sendSmtpEmail.htmlContent = `<html><body>${body}</body></html>`;
-    sendSmtpEmail.sender = { "name": "No Responder", "email": "no-reply@chelo.xyz" };
-    sendSmtpEmail.to = [to];
+    sendSmtpEmail.sender = { "email": mailSender };
+    sendSmtpEmail.to = [{ "email": to }];
     //sendSmtpEmail.cc = [{ "email": "example2@example2.com", "name": "Janice Doe" }];
     //sendSmtpEmail.bcc = [{ "name": "John Doe", "email": "example@example.com" }];
     //sendSmtpEmail.replyTo = { "email": "replyto@domain.com", "name": "John Doe" };
