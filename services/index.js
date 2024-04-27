eccc143e7582ccb190695779a63a3ccc68270d14
Chelo <me@chelo.xyz>
2024-04-27 12:51:46 -0500

    add MailsRepository

--- services/index.js
+++ services/index.js
@@ -1,10 +1,12 @@
 
-import { Users, Logs, Images } from '../dao/factory.js'
+import { Users, Logs, Images, Mails } from '../dao/factory.js'
 
 import UsersRepository from "./users.repository.js";
 import LogsRepository from "./logs.repository.js";
 import ImagesRepository from "./images.repository.js";
+import MailsRepository from "./mails.repository.js";
 
 export const UsersService = new UsersRepository(new Users());
 export const LogsService = new LogsRepository(new Logs());
 export const ImagesService = new ImagesRepository(new Images());
+export const MailsService = new MailsRepository(new Mails());
